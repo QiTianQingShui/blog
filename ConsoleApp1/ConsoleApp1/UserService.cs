a1c0bd74e904ef351e06901f3cdae165109b4237
DESKTOP-IH4G02L\jinnan <jinnan@outlook.com>
2026-08-10 07:38:06 +0800

    add openevals ariticels

---
+++ ConsoleApp1/ConsoleApp1/UserService.cs
@@ -0,0 +1,21 @@
+﻿//internal class ArticelReader
+//{
+//    public async Task<string> ReadAsync(string ariticeId)
+//    {
+//        var response = await new HttpClient().GetAsync($"https://example.com/ariticels/{ariticeId}");
+//        return await response.Content.ReadAsStringAsync();
+//    }
+//}
+
+internal class ArticelReader : IDisposable
+{
+    private HttpClient? _httpClient;
+    public async Task<string> ReadAsync(string ariticeId)
+    {
+        _httpClient ??= new HttpClient();
+        using var response = await _httpClient.GetAsync($"https://example.com/ariticels/{ariticeId}"));
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+    public void Dispose() => _httpClient?.Dispose();
+}
